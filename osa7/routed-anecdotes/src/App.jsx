d0cd6aaa42914a4b094adda54bf19023a1316599
Martin <martin.kalliola@gmail.com>
2024-01-25 23:39:59 +0200

    x

--- osa7/routed-anecdotes/src/App.jsx
+++ osa7/routed-anecdotes/src/App.jsx
@@ -3,7 +3,7 @@ import {
   Routes, Route, Link, useParams, useNavigate
 } from 'react-router-dom'
 import Notification from './components/Notification';
-
+import { useField } from './hooks/index'
 import { useState } from 'react'
 
 const Menu = () => {
@@ -73,28 +73,37 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
+  /*
   const [content, setContent] = useState('')
+  */
   const [author, setAuthor] = useState('')
   const [info, setInfo] = useState('')
-  const navigate = useNavigate()
+  
+  const navigate = useNavigate() 
+
+  const content = useField('text')
+  //const author = useField('text')
+  //const info = useField('text')
+
 
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
-    })
-    setContent('')
-    setAuthor('')
-    setInfo('')
-    navigate('/')
-    props.setNotification(`A new anecdote '${content}' was created`); // Update the notification message
+    });
+
+    // Resetting the form fields
+    
+    navigate('/');
+    props.setNotification(`A new anecdote '${content.value}' was created`);
     setTimeout(() => {
-      props.setNotification(''); // Clear the notification after a delay
-    }, 5000);  }
+      props.setNotification('');
+    }, 5000);
+  };
 
   return (
     <div>
@@ -102,22 +111,22 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} /> 
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} /> 
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} /> 
         </div>
         <button>create</button>
       </form>
     </div>
-  )
+  );
+};
 
-}
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
